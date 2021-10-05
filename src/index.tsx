2b414880f765d5230f5ea2ceff9ae9a44251b326
George <71434803+george-swift@users.noreply.github.com>
2021-10-06 00:07:43 +0100

    Wrap App component in Provider

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom';
+import './stylesheets/index.css';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
